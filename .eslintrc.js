ef25ae59b4021925e442dc8b059e1c4ebd74fe89
d_kudrin <d_kudrin@gamma.kz>
2020-11-20 17:15:30 +0600

    init vue app + docker

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,22 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    browser: true,
+    node: true
   },
   extends: [
     'plugin:vue/essential',
     '@vue/airbnb',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended'
   ],
+  plugins: ['prettier'],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
-};
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
