4ceac41ce77a3b616a9c9d32c0d7a2a4f096f3e5
d_kudrin <d_kudrin@gamma.kz>
2020-11-21 18:32:28 +0600

    fix api url

--- src/api/httpClient.js
+++ src/api/httpClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://jservice.io',
+  baseURL: 'https://jservice.io',
   headers: {
     'Content-Type': 'application/json',
   },
